4c363bb91bf3f7e35e5b4854a4382ad8f549f88f
Ignacio Inostroza <iinostroza1@miuandes.cl>
2021-03-25 22:36:31 -0300

    Lab1 ver0.6

--- Laboratorio 1. poo .py
+++ Laboratorio 1. poo .py
@@ -1,5 +1,5 @@
 import random
-
+#ver Lab1 ver0.6
 #Crea una lista con los numeros asociados a cada carta
 def cardmaker(num):
     i = 1
@@ -22,29 +22,16 @@ def checker(num1,num2):
 
 #Checkea la condicion del juego: #1 si el jugador 1 gana #2 si el jugador 2 gana #3 si se produce un empate #0 si el juego continua
 def wincondition(total,player1,player2):
-    a = (total/2)
+    a = int((total/2))
     win = 0
-    if player1 > a:
+    if (player1) > (a):
         win = 1
     if player2 > a:
         win = 2
     if player1 == player2 == a:
         win = 3
-    if player1 != player2 != a:
-        win = 0
     return win
 
-#Remueve la carta de la lista total, cuando un jugador logra una correcta
-def cardremove(card,lista):
-    a = []
-    i = 0
-    while i < len(lista):
-        if lista[i] != card:
-            a.append(lista[i])
-            i+=1
-        else:
-            i+=1
-    return a
 #Funcion encargada de organizar las cartas y asignarles coord
 def cardlist(cartas,total):
     i = 0
@@ -52,61 +39,21 @@ def cardlist(cartas,total):
     a = int(total/2)
     part1 = []
     part2 = []
+    part1b = []
+    part2b = []
 
     while i < a:
-        part1.append([cartas[i],("1,"+ str(i+1))])
+        part1.append(cartas[i])
+        part1b.append("1,"+ str(i+1))
         i+=1
     while a < total:
-        part2.append([cartas[a],("2,"+ str(i2+1))])
+        part2.append(cartas[a])
+        part2b.append("2,"+ str(i2+1))
         i2+=1
         a+=1
-    return part1,part2
-#Funcion que printea un tablero según lista de cartas
-def printboard(part1,part2,num):
-    i=0
-    i2=0
-    cardstring = ""
-    if num != "?":
-        while i < len(part1):
-            value =  part1[i][1] 
-            if value == num:
-                cardstring = cardstring + " |" + str(part1[i][0]) + "| "
-                i+=1
-            if value != num:
-                cardstring = cardstring + " |?| "
-                i+=1
-        cardstring = cardstring + "\n"
-        while i2 < len(part2):
-            value = part2[i2][1]
-            if value == num:
-                cardstring = cardstring + " |" + str(part1[i2][0]) + "| "
-                i2+=1
-            if value != num:
-                cardstring = cardstring + " |?| "
-                i2+=1
-    else:
-        while i < len(part1):
-            value = part1[i][0]
-            cardstring = cardstring + " |?| "
-            i+=1
-        cardstring = cardstring + "\n"
-        while i2 < len(part2):
-            value = part2[i2][0]
-            cardstring = cardstring + " |?| "
-            i2+=1
-    return cardstring
-#Cambia valor de carta ya fuera de juego
-def pointcheck(value,list1):
-    i=0
-    list2 = []
-    while i < len(list1):
-        if value == list1[i][0]:
-            list2.append(["X",list1[i][1]])
-            i+=1
-        else:
-            list2.append(list1[i])
-            i+=1
-    return list2
+    return part1,part2,part1b,part2b
+
+
 #######################################################################
 
 a = int(input("Ingrese número de cartas a jugar por jugador\n"))
@@ -117,14 +64,12 @@ player1score = 0
 player2score = 0
 
 cartas = cardmaker(a)
-total = len(cartas)
+total = int(len(cartas)/2)
 
 #Bucle de juego principal
 win = 0
 choice = 0
 choice2 = 0
-part1 = (cardlist(cartas,total))[0]
-part2 = (cardlist(cartas,total))[1]
 
 while win == 0:
     if playerturn1 == 1:
@@ -160,14 +105,21 @@ while win == 0:
             playerturn2 = 1
             print(player2score)
             print(win)
+    if win == 1:
+        print("Gana jugador 1")
+        break
+    if win == 2:
+        print("Gana jugador 2")
+        break
+    if win == 3:
+        print("Empate")
+        break
+        
+        
+        
+    
+    
 
-    else:
-        pass
-        
-        
-        
-    
-    
 
 
 
